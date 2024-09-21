a8744e70ec33abc148dac8904dbd6e453010213f
cardon <philantrope1@outlook.com>
2024-09-21 19:06:53 +0200

    index et increment

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -1,4 +1,15 @@
 class ClicksController < ApplicationController
   def index
+    session[:counter] || = 0
+  end
+
+  def increment
+    session[:counter] || += 1
+    redirect_to root_path
+  end
+
+  def reset
+    session[:counter] = 0
+    redirect_to root_path
   end
 end
